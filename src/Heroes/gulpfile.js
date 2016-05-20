f6b4c5c6832d6b7aa39e541291e50e25971b7870
Patch <patchandthat@gmail.com>
2016-05-20 13:34:22 +0100

    Part II

--- src/Heroes/gulpfile.js
+++ src/Heroes/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='clean' AfterBuild='copy-scripts, copy-styles' Clean='clean' />
+/// <binding Clean='clean' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
